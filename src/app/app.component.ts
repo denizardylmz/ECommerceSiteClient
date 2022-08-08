b9e6813713fa42996a5a8bbb0d1303f82246ffb9
denizyilmaz <deniz-arda-yilmaz@outlook.com>
2022-08-08 16:16:38 +0300

    Commit_1

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { CustomToastrService, ToastrMessageType, ToastrOptions, ToastrPosition } from './services/ui/custom-toastr.service';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'ETicaretClient';
+
+  //Uses CustomToastr Service for customized notifications
+  constructor(){}
+
+
+
+}
+
